4a70a5137df008213ac43e80d20a6a7b741fe72e
Martin-Raby <martin.raby@orange.fr>
2025-04-01 20:36:36 +0200

    Ajout de l'option de tri.

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ import { affichage_resultats } from "./modules/affichage_resultats.mjs";
 "use strict";
 
 window.addEventListener("DOMContentLoaded", function(){
-    $("button.bouton_recherche").on("click", async function(){await affichage_resultats( $("input.barre_recherche").val() ) });
+    $("button.bouton_recherche").on("click", async function(){await affichage_resultats( $("input.barre_recherche").val(), $("select.choix_tri").val()) });
     $(document).on('keypress',async function(e) {
         if(e.which == 13) {
-            await affichage_resultats( $("input.barre_recherche").val() )
+            await affichage_resultats( $("input.barre_recherche").val(), $("select.choix_tri").val() )
         }
     });
 })
--- modules/affichage_resultats.mjs
+++ modules/affichage_resultats.mjs
@@ -1,7 +1,7 @@
 import { requete_api } from "./requete.mjs";
 "use strict";
 
-async function affichage_resultats (texte) {
+async function affichage_resultats (texte, donnee) {
     /*
     Entrée : texte une chaîne de caractères
     Sortie : rien
@@ -14,7 +14,8 @@ async function affichage_resultats (texte) {
 
     let contenu = ""
     await requete_api(texte).then( (resultat) => {
-        const images = resultat.result.images
+        const images = tri_images(resultat.result.images, donnee)
+        
         for (let i = 0; i < images.length; i++){
             contenu += `<img src="${images[i].element_url}" alt="${images[i].name}"/>`
         }
@@ -23,6 +24,30 @@ async function affichage_resultats (texte) {
         contenu = "<p>Aucun résultat</p>"
     }
     $("div.espace_resultats").html(contenu)
+}
+
+function tri_images (images, donnee = "name"){
+    switch (donnee){
+        case "name" :
+            images.sort((a, b) => a.name.localeCompare(b.name));
+            break;
+        case "hit" :
+            images.sort((a, b) => b.hit - a.hit);
+            break;
+        case "!date_creation" :
+            images.sort((a, b) => new Date(b.date_creation) - new Date(a.date_creation));
+            break;
+        case "!date_available" :
+            images.sort((a, b) => new Date(b.date_available) - new Date(a.date_available));
+            break;
+        case "date_creation" :
+            images.sort((a, b) => new Date(a.date_creation) - new Date(b.date_creation));
+            break;
+        case "date_available" :
+            images.sort((a, b) => new Date(a.date_available) - new Date(b.date_available));
+            break;
+    }
+    return images
 }
 
 export { affichage_resultats }
