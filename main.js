97e8e0c0b38b6536237ced05cafc7a9000d3c710
Martin-Raby <martin.raby@orange.fr>
2025-04-01 17:22:38 +0200

    L'affichage des images est propre. La recherche se fait également si la touch entré est pressée.

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ import { affichage_resultats } from "./modules/affichage_resultats.mjs";
 
 window.addEventListener("DOMContentLoaded", function(){
     $("button.bouton_recherche").on("click", async function(){await affichage_resultats( $("input.barre_recherche").val() ) });
+    $(document).on('keypress',async function(e) {
+        if(e.which == 13) {
+            await affichage_resultats( $("input.barre_recherche").val() )
+        }
+    });
 })
