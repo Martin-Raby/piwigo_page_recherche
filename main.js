f152706082bb9379a340d3433ba06900cb02d9f8
E213872U <martin.raby@etu.univ-nantes.fr>
2025-04-03 15:52:23 +0200

    Ajout du choix d'un tag (sans conséquences sur la recherche)

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { affichage_resultats } from "./modules/affichage_resultats.mjs";
+import { populate_tags } from "./modules/gestion_tags.mjs";
 "use strict";
 
 window.addEventListener("DOMContentLoaded", function(){
@@ -9,4 +10,5 @@ window.addEventListener("DOMContentLoaded", function(){
         }
     });
     $("select.choix_tri").on("change", async function(){await affichage_resultats( $("input.barre_recherche").val(), $("select.choix_tri").val()) });
+    populate_tags()
 })
---
+++ modules/gestion_tags.mjs
@@ -0,0 +1,18 @@
+import { tag_api } from "./requete.mjs"
+"use strict";
+
+async function populate_tags() {
+    let liste_tags = (await tag_api()).result.tags;
+    $("select.choix_tag").html(`<option value="None">Aucun tag</option>`)
+    
+    for (let i = 0; i < liste_tags.length; i++){
+
+        $("select.choix_tag").append($('<option>', {
+            value: liste_tags[i].url_name,
+            text: liste_tags[i].name
+        }));
+
+    }
+}
+
+export { populate_tags }
